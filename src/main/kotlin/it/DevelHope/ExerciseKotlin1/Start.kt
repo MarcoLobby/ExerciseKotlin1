48cba0d717f933f775c94c0c4c5b8d1d63ca1d28
Marco Lo Bello <xSalamander@outlook.it>
2023-01-08 16:39:01 +0100

    Initial Commit

---
+++ src/main/kotlin/it/DevelHope/ExerciseKotlin1/Start.kt
@@ -0,0 +1,11 @@
+package it.DevelHope.ExerciseKotlin1
+
+//Declare 3 different variables and print their values using string templates
+
+fun main(){
+    var number1 = 1;
+    var number2 = 2;
+    var number3 = 3;
+
+    println("Number 1 is: $number1.\nNumber 2 is: $number2.\nNumber 3 is: $number3.")
+}
